97d5efa8a52829c622a16d9c0a29080f20b859a8
Woong <kweyz@daum.net>
2021-10-04 21:41:13 +0900

    Search Controller

--- src/init.js
+++ src/init.js
@@ -5,6 +5,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`Server Listening on port http://localhost:${PORT} 🔥`);
+  console.log(`🔥 Server Listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening); // listen(port, callback)
