b1d25e568004fe4c69233b86400634fbe8b94520
Woong <kweyz@daum.net>
2021-10-13 21:04:45 +0900

    Proctor and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,3 +5,20 @@ export const localsMiddleware = (req, res, next) => {
   // console.log(res.locals);
   next();
 };
+
+// 유저가 loggedIn 돼 있으면, 요청을 계속하고 없으면 로그인페이지로 되돌아감
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+// 유저가 로그아웃 돼 있어야 실행시키는 것을 허락
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
