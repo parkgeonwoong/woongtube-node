1b5a01723ff17c1a3f16485ab5eb7e37e814ae0b
Woong <kweyz@daum.net>
2021-11-01 01:35:01 +0900

    User VideoArray .populate

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -53,14 +53,17 @@ export const postUpload = async (req, res) => {
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
-    await Video.create({
+    const newVideo = await Video.create({
       title: title,
       description,
       fileUrl,
       owner: _id,
-      // createdAt: Date.now(),
       hashtags: Video.formatHashtags(hashtags),
     });
+    const user = await User.findById(_id);
+    user.videos.push(newVideo._id);
+    user.save();
+
     return res.redirect("/");
   } catch (error) {
     return res.status(400).render("upload", {
--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   name: { type: String, required: true },
   location: String,
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
 // 비밀번호 저장 bcrypt
