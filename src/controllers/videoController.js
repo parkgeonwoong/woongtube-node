668aeb5185594049ce8d2a11e3b77a407c60f9a8
Woong <kweyz@daum.net>
2021-11-06 18:16:05 +0900

    Client SCSS: header&footer&video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,9 @@ import User from "../models/User";
 
 // 메인 홈
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
