bf4ea9fb18f0e3f428d0363531e764fed36637cd
Woong <kweyz@daum.net>
2021-11-18 22:18:35 +0900

    Downloading VideoFile

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,9 +3,16 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
 // 동영상 다운로드
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 
 // 동영상 멈춤 버튼
 const handleStop = () => {
@@ -23,11 +30,11 @@ const handleStart = () => {
   startBtn.addEventListener("click", handleStop);
 
   // 기록하는 개체 stop 이후 생성
-  recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
     console.log("recording End");
     // console.log(event.data);
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     // console.log(videoFile);
     video.srcObject = null;
     video.src = videoFile;
