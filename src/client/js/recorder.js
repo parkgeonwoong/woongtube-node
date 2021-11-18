b382ce6ea612fcd299651d8e90b95b479461621f
Woong <kweyz@daum.net>
2021-11-18 21:33:30 +0900

    Recording videoUrl & show

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,12 +2,18 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
+let recorder;
+
+// 동영상 다운로드
+const handleDownload = () => {};
 
 // 동영상 멈춤 버튼
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleDownload);
+
+  recorder.stop();
 };
 
 // 동영상 시작 버튼
@@ -16,18 +22,19 @@ const handleStart = () => {
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
 
-  const recorder = new MediaRecorder(stream);
+  // 기록하는 개체 stop 이후 생성
+  recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     console.log("recording End");
-    console.log(event);
-    console.log(event.data);
+    // console.log(event.data);
+    const videoFile = URL.createObjectURL(event.data);
+    // console.log(videoFile);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
-  console.log(recorder);
   recorder.start();
-  console.log(recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 // 미리보기 동영상 녹화
